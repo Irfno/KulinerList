a331e26617c721ddcb1c88706dfdac98d1a50b40
rxprasetya <irfno25@gmail.com>
2023-11-19 18:38:15 +0700

    [BAB 6] Animation

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -1,13 +1,18 @@
-import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Image, ScrollView, Animated } from 'react-native'
+import React, { useRef } from 'react'
 import { ArrowLeft, More } from 'iconsax-react-native'
+import { useNavigation } from '@react-navigation/native';
 
 export default function Detail() {
+    const nav = useNavigation()
+
+    const scrollY = useRef(new Animated.Value(0)).current
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <View style={styles.headerStart}>
-                    <TouchableOpacity style={styles.back}>
+                    <TouchableOpacity style={styles.back} onPress={() => nav.navigate('HomeScreens')}>
                         <ArrowLeft variant='Linear' color='grey' size={20} />
                     </TouchableOpacity>
                     <Text style={styles.detailText}>Detail</Text>
@@ -16,12 +21,22 @@ export default function Detail() {
                     <More variant='Linear' color='grey' size={24} rotation={90} />
                 </View>
             </View>
-            <Image style={styles.imageStyle} source={require('../images/pekpek.jpg')} />
-            <View style={styles.cardContent}>
-                <Text style={styles.titleText}>Pempek - Palembang</Text>
-                <Text style={styles.descText}>Pempek adalah makanan yang dibuat dari daging ikan yang digiling lembut yang dicampur tepung kanji atau tepung sagu, serta komposisi beberapa bahan lain seperti telur, bawang putih yang dihaluskan, penyedap rasa, dan garam. Pempek biasanya disajikan dengan kuah yang disebut cuka yang memiliki rasa asam, manis, dan pedas. Pempek merupakan makanan khas Palembang - Sumatera Selatan. Pada tahun 1880-an, penjual biasa memikul satu keranjang penuh pempek sambil berjalan kaki berkeliling menjajakan dagangannya. Di kota nya pempek , palembang. cara makan pempek yang benar adalah dengan mengunakan mangkuk kecil sebagai tempat cukonya lalu di makan pempeknya setelah itu pempek dicocol dan cukonya diseruput untuk menambah rasanya nikmatnya.</Text>
-            </View>
-        </View>
+            <Animated.ScrollView
+                style={[styles.cardContent,]}
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                    { useNativeDriver: true },
+                )}>
+                <Animated.Image style={styles.imageStyle(scrollY)} source={require('../images/pekpek.jpg')} />
+                <View style={styles.cardText}>
+
+                    <Text style={styles.titleText}>Pempek - Palembang</Text>
+                    <Text style={styles.descText}>Pempek adalah makanan yang dibuat dari daging ikan yang digiling lembut yang dicampur tepung kanji atau tepung sagu, serta komposisi beberapa bahan lain seperti telur, bawang putih yang dihaluskan, penyedap rasa, dan garam. Pempek biasanya disajikan dengan kuah yang disebut cuka yang memiliki rasa asam, manis, dan pedas. Pempek merupakan makanan khas Palembang - Sumatera Selatan. Pada tahun 1880-an, penjual biasa memikul satu keranjang penuh pempek sambil berjalan kaki berkeliling menjajakan dagangannya. Di kota nya pempek , palembang. cara makan pempek yang benar adalah dengan mengunakan mangkuk kecil sebagai tempat cukonya lalu di makan pempeknya setelah itu pempek dicocol dan cukonya diseruput untuk menambah rasanya nikmatnya.</Text>
+                    <Text style={styles.descText}>Pempek adalah makanan yang dibuat dari daging ikan yang digiling lembut yang dicampur tepung kanji atau tepung sagu, serta komposisi beberapa bahan lain seperti telur, bawang putih yang dihaluskan, penyedap rasa, dan garam. Pempek biasanya disajikan dengan kuah yang disebut cuka yang memiliki rasa asam, manis, dan pedas. Pempek merupakan makanan khas Palembang - Sumatera Selatan. Pada tahun 1880-an, penjual biasa memikul satu keranjang penuh pempek sambil berjalan kaki berkeliling menjajakan dagangannya. Di kota nya pempek , palembang. cara makan pempek yang benar adalah dengan mengunakan mangkuk kecil sebagai tempat cukonya lalu di makan pempeknya setelah itu pempek dicocol dan cukonya diseruput untuk menambah rasanya nikmatnya.</Text>
+                    <Text style={styles.descText}>Pempek adalah makanan yang dibuat dari daging ikan yang digiling lembut yang dicampur tepung kanji atau tepung sagu, serta komposisi beberapa bahan lain seperti telur, bawang putih yang dihaluskan, penyedap rasa, dan garam. Pempek biasanya disajikan dengan kuah yang disebut cuka yang memiliki rasa asam, manis, dan pedas. Pempek merupakan makanan khas Palembang - Sumatera Selatan. Pada tahun 1880-an, penjual biasa memikul satu keranjang penuh pempek sambil berjalan kaki berkeliling menjajakan dagangannya. Di kota nya pempek , palembang. cara makan pempek yang benar adalah dengan mengunakan mangkuk kecil sebagai tempat cukonya lalu di makan pempeknya setelah itu pempek dicocol dan cukonya diseruput untuk menambah rasanya nikmatnya.</Text>
+                </View>
+            </Animated.ScrollView>
+        </View >
     )
 }
 
@@ -31,9 +46,15 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
     },
     header: {
+        position: 'absolute',
+        zIndex: 1000,
+        top: 0,
+        left: 0,
+        right: 0,
         flexDirection: 'row',
         height: 64,
         paddingHorizontal: 8,
+        backgroundColor: '#FFF',
     },
     headerStart: {
         flex: 1,
@@ -55,24 +76,40 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginLeft: 4,
     },
-    imageStyle: {
+    imageStyle: scrollY => ({
         resizeMode: 'contain',
         height: 300,
         width: 425,
         borderRadius: 16,
         alignSelf: 'center',
-    },
+        transform: [{
+            translateY: scrollY
+        },
+        {
+            scale: scrollY.interpolate({
+                inputRange: [-300, 0],
+                outputRange: [2, 1],
+            })
+        },
+        ],
+    }),
 
     cardContent: {
-        padding: 16,
+        paddingHorizontal: 16,
+        marginTop: 72,
+    },
+
+    cardText: {
+        backgroundColor: '#FFF',
     },
     titleText: {
         color: '#000',
         fontSize: 20,
         fontWeight: '700',
+        // marginLeft: 16,
+        marginVertical: 8,
     },
     descText: {
-        marginTop: 4,
         color: '#A3A3A3',
         fontSize: 16,
         textAlign: 'justify',
